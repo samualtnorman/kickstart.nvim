5dd1e28be1bbc89978eaa959487da5dc8dba7e3a
Samual <me@samual.uk>
2024-05-16 23:36:04 +0100

    auto clean unused language servers

--- init.lua
+++ init.lua
@@ -667,7 +667,11 @@ require('lazy').setup({
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
       })
-      require('mason-tool-installer').setup { ensure_installed = ensure_installed }
+
+      local MasonToolInstaller = require 'mason-tool-installer'
+
+      MasonToolInstaller.setup { ensure_installed = ensure_installed }
+      MasonToolInstaller.clean()
 
       require('mason-lspconfig').setup {
         handlers = {
