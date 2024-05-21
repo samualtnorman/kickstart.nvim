eb50df72da40da5dc88a62cfb7b3f72930706d08
Samual Norman <me@samual.uk>
2024-05-21 09:02:02 +0100

    disable scrolloff

--- init.lua
+++ init.lua
@@ -158,7 +158,7 @@ vim.opt.inccommand = 'split'
 vim.opt.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+--vim.opt.scrolloff = 10
 
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 0
