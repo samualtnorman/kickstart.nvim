a17d66daa8005a444d690128b7f0f00547364c82
Samual Norman <me@samual.uk>
2024-05-21 09:02:40 +0100

    add JSON language server

--- init.lua
+++ init.lua
@@ -611,6 +611,7 @@ require('lazy').setup({
         nil_ls = {},
         dockerls = {},
         docker_compose_language_service = {},
+        jsonls = {},
       }
 
       -- Ensure the servers and tools above are installed
